3c67bd5a7e2ce2e5b1b9a3fec6b3e6c277b286e5
Unknown <dominik.wieland@student.tugraz.at>
2018-04-29 20:37:55 +0200

    renamed vector for ouput arguments

--- desired_rand.m
+++ desired_rand.m
@@ -1,18 +1,18 @@
-function [ output_args ] = desired_rand( n, seed, delta_x, mu )
+function [ rnd_number_vector, acceptance_rate ] = desired_rand( n, seed, delta_x, mu )
 %DESIRED_RAND Summary of this function goes here
 %   Detailed explanation goes here
-    output_args = zeros([1, n]);
-    output_args(1) = seed;
+    rnd_number_vector = zeros([1, n]);
+    rnd_number_vector(1) = seed;
     n_accepted = 0;
+    
     for i = 2:n
         r = rand();
-        output_args(i) = output_args(i-1) + (r - 0.5) * delta_x;
-
-        acceptance_probability = desired_pdf(output_args(i), mu) / desired_pdf(output_args(i-1), mu);
+        rnd_number_vector(i) = rnd_number_vector(i-1) + (r - 0.5) * delta_x;
+        acceptance_probability = desired_pdf(rnd_number_vector(i), mu) / desired_pdf(rnd_number_vector(i-1), mu);
         if acceptance_probability < 1
             random_accept = rand();
             if random_accept > acceptance_probability
-                output_args(i) = output_args(i-1); 
+                rnd_number_vector(i) = rnd_number_vector(i-1); 
             else
                 n_accepted = n_accepted + 1;
             end
@@ -23,7 +23,6 @@ function [ output_args ] = desired_rand( n, seed, delta_x, mu )
     end
     
     acceptance_rate = n_accepted/n;
-    fprintf('Acceptance rate: %f\n',acceptance_rate);
  
 end
 
